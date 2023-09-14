e381fd20e424c9ba4ec8ce73cd7c52429d4ead5e
DARP-MAKER <ingenierodiegorios@gmail.com>
2023-09-14 19:41:56 -0400

    carga entrega final

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
+function obtenerTasaInteres(datos) {
+    if (datos.length > 0) {
+      datos.sort((a, b) => (a.Date < b.Date ? 1 : -1));
+      const tasaInteresMasReciente = datos[0].Value;
+      document.getElementById("tasaInteres").textContent = `Tasa de interés vigente de la reserva federal de los Estados Unidos: ${tasaInteresMasReciente}%`;
+    } else {
+      document.getElementById("tasaInteres").textContent = "No se encontraron datos de tasa de interés.";
+    }
+  }
+  
+  function cargarDatosDesdeJSON() {
+    const archivoJSON = "nasdaq.json";
+    fetch(archivoJSON)
+      .then((response) => response.json())
+      .then((data) => obtenerTasaInteres(data))
+      .catch((error) => console.error("Error al obtener datos desde el archivo JSON:", error));
+  }
+  
+  function cargarDatosDesdeAPI() {
+    const urlAPI = "https://data.nasdaq.com/api/v3/datasets/FRED/DTB3.csv?api_key=sk7WcR_LiaqihqK2aUTs";
+    fetch(urlAPI)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error al cargar datos desde la API");
+        }
+        return response.json();
+      })
+      .then((data) => obtenerTasaInteres(data))
+      .catch(() => {
+        console.error("Error al obtener datos desde la API. Cargando desde JSON después de 5 segundos.");
+        setTimeout(cargarDatosDesdeJSON, 5000); // Intenta cargar desde JSON después de 5 segundos
+      });
+  }
+  
+  cargarDatosDesdeAPI();
+  
 class Loan {
     constructor(capital, cuotas, interes) {
         this.capital = capital;
@@ -62,20 +98,20 @@ function mostrarResultados() {
             </tr>`).join('');
 
         resultadoDiv.innerHTML = `
-            <h2>Resultados del préstamo ${index + 1}</h2>
+            <h2>Resultados del crédito ${index + 1}</h2>
             <table>
                 <thead>
                     <tr>
                         <th>Cuota</th>
                         <th>Abono Capital</th>
-                        <th>Interés Mensual</th>
+                        <th>Abono intereses</th>
                         <th>Saldo</th>
                     </tr>
                 </thead>
                 <tbody>${amortizacionFormatted}</tbody>
             </table>
             <p>Total Interés: ${resultado.totalInteres.toFixed(2)}</p>
-            <p>Total Préstamo: ${resultado.totalPrestamo.toFixed(2)}</p>
+            <p>Total crédito: ${resultado.totalPrestamo.toFixed(2)}</p>
         `;
 
         resultadosDiv.appendChild(resultadoDiv);
@@ -94,9 +130,29 @@ document.getElementById("calcular").addEventListener("click", function() {
     const cuotas = parseInt(document.getElementById("cuotas").value);
     const interes = parseFloat(document.getElementById("interes").value);
 
+    if (isNaN(capital) || isNaN(cuotas) || isNaN(interes)) {
+        // Mostrar una alerta de error con SweetAlert2 si los campos no son válidos
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Por favor, ingrese números válidos en todos los campos.',
+        });
+        return;
+    }
+   
     agregarPrestamo(capital, cuotas, interes);
     mostrarResultados();
     guardarPrestamosEnStorage();
+
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: '¡Aquí está el cálculo de tu crédito!',
+        showConfirmButton: false,
+        timer: 3000
+
+    });
+    
 });
 
 document.getElementById("limpiar").addEventListener("click", function() {
@@ -118,10 +174,5 @@ function cargarPrestamosDesdeStorage() {
 }
 
 cargarPrestamosDesdeStorage();
-
-
-
-
-
 
 
